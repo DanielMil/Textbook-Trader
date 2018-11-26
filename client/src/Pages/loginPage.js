2e348284ebddfdf520f23d95ea4bbfd36d026e03
DanielMilGuelph <dmil@mail.uoguelph.ca>
2018-11-26 18:59:59 -0500

    Minor style changes

--- client/src/Pages/loginPage.js
+++ client/src/Pages/loginPage.js
@@ -4,6 +4,7 @@ import Icon from 'react-icons-kit';
 import {mail} from 'react-icons-kit/entypo/mail';
 import {lock} from 'react-icons-kit/fa/lock';
 import "../Styles/Login.css";
+import { CardImg } from 'reactstrap';
 
 function setErrorMsg(error) {
   return {
@@ -29,6 +30,12 @@ export default class Login extends Component {
     return (
       <div className="col-sm-10 col-sm-offset-1 content align-middle">
         <row>
+
+          <div className="col-lg-6">
+            <h1>Radl</h1>
+            <CardImg top width="75%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap"/>
+          </div>
+
           <div className="col-lg-6">
           <h1 className="contentHeading">Sign in</h1>
           <form onSubmit={this.handleSubmit}>
@@ -53,10 +60,6 @@ export default class Login extends Component {
           <div className="col-xs-8 col-xs-offset-2">
           <h4 className="contentText">Not a member? <a href="/register">Sign up now</a></h4>
           </div>
-          </div>
-
-          <div className="col-lg-6">
-            <h1>where's the fkin blo(go)w</h1>
           </div>
 
         </row>
